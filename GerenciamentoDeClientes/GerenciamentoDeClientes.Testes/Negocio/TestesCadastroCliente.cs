a5cb68c0fa88b6afae1c5a70373842e95f2bf9d5
clarisse.heleno <clarisse.heleno@NOTE-CLARISSE>
2018-06-12 11:34:10 -0300

    Teste de unidade do método ApagaCliente,

---
+++ GerenciamentoDeClientes/GerenciamentoDeClientes.Testes/Negocio/TestesCadastroCliente.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using GerenciamentoDeClientes.Dados.Contratos;
+using GerenciamentoDeClientes.Dominio;
+using GerenciamentoDeClientes.Negocio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GerenciamentoDeClientes.Testes.Negocio
+{
+    [TestClass]
+    public class TestesCadastroCliente
+    {
+        [TestMethod]
+        public void ApagaClienteSemVenda()
+        {
+            var mockCliente = new Mock<IRepositorioCliente>();
+            var mockVendas = new Mock<IRepositorioVenda>();
+
+            var cadastroCliente = new CadastroCliente(mockCliente.Object, mockVendas.Object);
+            var retorno = cadastroCliente.ApagaCliente(1);
+
+            Assert.IsTrue(retorno);
+            mockCliente.Verify(r => r.ApagaCliente(1));
+        }
+
+        [TestMethod]
+        public void ApagaClienteComVenda()
+        {
+            var mockCliente = new Mock<IRepositorioCliente>();
+            var mockVendas = new Mock<IRepositorioVenda>();
+            mockVendas.Setup(r => r.BuscaVendasPorCliente(1)).Returns((int codigo) => new[] { new Venda() });
+
+            var cadastroCliente = new CadastroCliente(mockCliente.Object, mockVendas.Object);
+            var retorno = cadastroCliente.ApagaCliente(1);
+
+            Assert.IsFalse(retorno);
+            mockCliente.VerifyNoOtherCalls();
+        }
+    }
+}
